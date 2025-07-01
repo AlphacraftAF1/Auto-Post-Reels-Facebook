67bb5d49025960acc57741ccf4372e289c11bedc
AlphacraftAF1 <alphagaming598@gmail.com>
2025-07-01 21:15:43 +0800

    Update gemini_processor.py

--- gemini_processor.py
+++ gemini_processor.py
@@ -1,6 +1,7 @@
 import google.generativeai as genai
 import random
 import logging
+import os
 
 # Konfigurasi logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
